68b4f5cfa9343d5e6966935accbf64cf7cb7d979
Joani COUMA <joani.couma@gmail.com>
2014-02-10 21:54:22 +0100

    Little fixes in status template

--- app/templates/status.php
+++ app/templates/status.php
@@ -18,7 +18,7 @@
 
         <h1>You are reading article <?=  $item->getId() ?> </h1>
         <p>
-            <p>It has been writen by <?= $item->getOwner() ?></p>
+            <p>It has been writen by <?= $item->getOwner()->get()['pseudo'] ?></p>
             <p>It has been published on the <?= date_format($item->getDate(), 'd/m/Y g:i A') ?></p>
             <blockquote><?= $item->getText() ?></blockquote>
         </p>
